e224e8f27bb25feee687013826c01d2d90812b4f
nhegm <nhegmus@gmail.com>
2023-04-19 20:18:54 +0800

    reformated code

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ public class Main {
 
         int account = 213;
         int fill = 550;
-        int bonus = fill/100;
-        int withBonus = account+bonus+fill;
-        int noBonus = account+fill;
+        int bonus = fill / 100;
+        int withBonus = account + bonus + fill;
+        int noBonus = account + fill;
 
         if (fill > 1000) {
-            System.out.println("Остаток на счете: "+withBonus+" рублей. Из них "+bonus+" бонусных рублей");
+            System.out.println("Остаток на счете: " + withBonus + " рублей. Из них " + bonus + " бонусных рублей");
         } else {
-            System.out.println("Остаток на счете: "+noBonus+" рублей.");
+            System.out.println("Остаток на счете: " + noBonus + " рублей.");
         }
     }
 }
